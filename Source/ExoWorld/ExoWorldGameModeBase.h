7b0ab9dc71829eb0c22cec4f4a885d3b592aee86
Sergey Lola <sergey_lola@mail.ru>
2022-06-21 00:17:49 +0300

    Created blank c++/blueprints project and configured control keys

---
+++ Source/ExoWorld/ExoWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExoWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXOWORLD_API AExoWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
